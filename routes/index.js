bd759626528851ff72f93e8b88c75d3c57cf82fe
Usama Waheed <usamaw94@gmail.com>
2018-08-19 18:40:35 +1000

    More

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const mongodb = require('mongodb');
+const Log = require('../models/log');
 
 router.get("/", function(req, res){
     res.render("index", { title: 'Express', data : 'some text' });
+})
+
+router.get('/alllogs', function(req,res){
+    var query = Log.find().sort('-_id');
+    query.exec(function(err,log){
+        //res.send(log);
+        res.render('logs',{log});
+    });
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,30 @@
+var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
 var expressLayouts = require('express-ejs-layouts');
 var app = express();
+
+var logger = require('morgan');
+var bodyparser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://usamaw94:uw123456@ds127342.mlab.com:27342/womdb', {useNewUrlParser: true});
+mongoose.Promise = global.Promise;
+
+var mongo = require('mongodb');
+
 
 //set port
 var port = process.env.PORT || 8000
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(bodyparser());
+app.use(cors());
 app.use(expressLayouts);
 app.use(express.static(__dirname + "/public"));
 
