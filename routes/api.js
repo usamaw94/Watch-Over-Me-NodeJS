0d09369de29340dea20a81183556f50989dd6dfb
waqas <waqax94@gmail.com>
2019-02-10 03:24:07 +1100

    More

--- routes/api.js
+++ routes/api.js
@@ -440,7 +440,7 @@ router.post('/receiveMessage', (req, res) => {
 
 function compareWatcherResponse(senderNum,serviceNum,response){
 
-    if(response == YES){
+    if(response === 'YES'){
         console.log(response);
         for(var i = 0 ; i < watcherResponses.length ; i++){
             if(JSON.stringify(watcherResponses[i].service_id) == serviceNum){
