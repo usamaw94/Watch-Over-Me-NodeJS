f4f21c72dcb66c0ae97996ea7f90923fab40a096
waqas <waqax94@gmail.com>
2019-02-10 02:53:52 +1100

    More

--- routes/api.js
+++ routes/api.js
@@ -447,6 +447,8 @@ function compareWatcherResponse(senderNum,serviceNum,response){
                     var phone = "+61" + watcherResponses[i].watchers[j].watcherPhone.substring(1);
                     if(JSON.stringify(phone) == senderNum){
                         watcherResponses[i].watchers[j].response = "true";
+
+                        console.log(JSON.stringify(watcherResponses));
                     }
                 }
             }
