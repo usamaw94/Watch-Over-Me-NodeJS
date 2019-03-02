d0d953a10cd5fa75933848c9adb420db6bff17c8
waqas <waqax94@gmail.com>
2019-03-02 23:00:29 +1100

    More

--- routes/api.js
+++ routes/api.js
@@ -347,7 +347,7 @@ function callingWatchers(i,regToken,log,tempData){
             }
 
             i++;
-            if (nextCall == true && i < tempData.watchers.length){
+            if (nextCall == true){
                 sendNotification("Connecting watcher","Watcher " +wCount+ " didn't respond","High",regToken);
                 if(i == tempData.watchers.length){
                     i = 0;
