47e8af67b49ce081bd1bd9f92bd64a475ece2a40
waqax94 <waqas.w2194@gmail.com>
2019-04-23 12:21:09 +1000

    More

--- public/userPanelAssets/js/custom.js
+++ public/userPanelAssets/js/custom.js
@@ -1,3 +1,33 @@
+function initialMap() {
+    //var lat = document.getElementById("latitude").value;
+    //var long = document.getElementById("longitude").value;
+    var wearerPosition = new google.maps.LatLng(-24.870193, 134.171259);
+    var mapOptions = {
+        center: wearerPosition,
+        zoom: 4,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+}
+
+function myMap(lat,long) {
+    //var lat = document.getElementById("latitude").value;
+    //var long = document.getElementById("longitude").value;
+    var wearerPosition = new google.maps.LatLng(lat,long);
+    var mapOptions = {
+        center: wearerPosition,
+        zoom: 15,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+    var marker = new google.maps.Marker({
+        position: wearerPosition,
+    });
+    marker.setMap(map);
+}
+
+//----------------------------------------------
+//----------------------------------------------
+
+
 $(".showCustomerDetails").on("click", function(){
 
     var customerId=$(this).attr('data-id');
